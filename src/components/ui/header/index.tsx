e88d3cb63989b8dc9bd20b06b5707ae978f67c04
iamtun <letuan19431791.iuh@gmail.com>
2024-06-08 09:55:32 +0700

    create header

---
+++ src/components/ui/header/index.tsx
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+import Link from 'next/link';
+
+import styles from './Header.module.scss';
+const Header = () => {
+  return (
+    <header className={styles.wrapper}>
+      <section className={styles.container}>
+        <div className={styles.logo}>Tuna</div>
+
+        <nav>
+          <Link href={'#'}>About</Link>
+          <Link href={'#'}>Experience</Link>
+          <Link href={'#'}>Projects</Link>
+          <Link href={'#'}>Contact</Link>
+        </nav>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/ui/index.tsx
@@ -0,0 +1,3 @@
+﻿import Header from './header';
+
+export {Header};
