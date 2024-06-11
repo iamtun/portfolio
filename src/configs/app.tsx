b41439394281e33b5351553337523a0d16cd0b42
iamtun <letuan19431791.iuh@gmail.com>
2024-06-11 14:01:46 +0700

    feat: add app config

---
+++ src/configs/app.tsx
@@ -0,0 +1,23 @@
+﻿import { BsLinkedin } from 'react-icons/bs';
+import { FaGithubAlt } from 'react-icons/fa';
+
+const configApp = {
+  logo: 'Tuna.dev',
+  hero: {
+    title: 'Front-End React Developer 👋',
+    desc: "Hi, I'm Tuna. A Front-end React Developer with 1.5 years of experience based in HCMC, VietNam. 📍",
+    socials: [
+      {
+        icon: <FaGithubAlt fontSize={28} />,
+        link: 'https://github.com/iamtun',
+      },
+      {
+        icon: <BsLinkedin fontSize={28} />,
+        link: 'https://www.linkedin.com/in/letuan-iuh',
+      },
+    ],
+    skills: ['html,css', 'js,ts', 'react,next', 'docker,scss'],
+  },
+};
+
+export default configApp;
