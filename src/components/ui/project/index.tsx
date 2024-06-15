27e49ffbdd3baab3f123cd46b0fbb3118bdbf607
iamtun <letuan19431791.iuh@gmail.com>
2024-06-15 14:53:57 +0700

    feat: create project component

---
+++ src/components/ui/project/index.tsx
@@ -0,0 +1,15 @@
+﻿import React from 'react';
+
+import styles from './Project.module.scss';
+
+const ProjectList: React.FC = () => {
+  return (
+    <section
+      className={styles.wrapper}
+      id="project"
+    >
+      <h1>New projects coming soon.. 💪</h1>
+    </section>
+  );
+};
+export default ProjectList;
