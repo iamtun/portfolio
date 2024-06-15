fb70d9541440449e53072bc714fc3e35d95c9a98
iamtun <letuan19431791.iuh@gmail.com>
2024-06-15 14:54:36 +0700

    feat: add contact and project to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import { About, Hero } from '@/components/ui';
+import { About, Contact, Hero, ProjectList } from '@/components/ui';
 
 export default function Home() {
   return (
     <>
       <Hero />
       <About />
+      <ProjectList />
+      <Contact />
     </>
   );
 }
--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -1,5 +1,7 @@
 ﻿import Header from './header';
 import Hero from './hero';
 import About from './about';
+import ProjectList from './project';
+import Contact from './contact';
 
-export { Header, Hero, About };
+export { Header, Hero, About, ProjectList, Contact };
