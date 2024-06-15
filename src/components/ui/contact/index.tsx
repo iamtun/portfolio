10b320ba43f1bd965a5f5eafd7bd1f9edddc5e97
iamtun <letuan19431791.iuh@gmail.com>
2024-06-15 14:54:14 +0700

    feat: create contact component

---
+++ src/components/ui/contact/index.tsx
@@ -0,0 +1,27 @@
+﻿import styles from './Contact.module.scss';
+import configApp from '@/configs/app';
+import Info from './info';
+
+const Contact: React.FC = () => {
+  return (
+    <section
+      className={styles.wrapper}
+      id="contact"
+    >
+      <div className={styles.container}>
+        <h2>{configApp.contact.title}</h2>
+        <h3>{configApp.contact.desc}</h3>
+        <div className={styles.content}>
+          {configApp.contact.infoList.map((info, idx) => (
+            <Info
+              {...info}
+              key={idx}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
---
+++ src/components/ui/contact/info/index.tsx
@@ -0,0 +1,22 @@
+﻿import React from 'react';
+import styles from './Info.module.scss';
+
+interface InfoProps {
+  icon: React.ReactNode;
+  label: string;
+  content: string;
+}
+
+const Info: React.FC<InfoProps> = ({ icon, label, content }) => {
+  return (
+    <div className={styles.container}>
+      <span className={styles.icon}>{icon}</span>
+      <div className={styles.text}>
+        <strong>{label}</strong>
+        <p>{content}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
