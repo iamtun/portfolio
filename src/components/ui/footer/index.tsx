3fba08ff9d814809d610f001e7766be0e3178b88
iamtun <letuan19431791.iuh@gmail.com>
2024-06-15 15:16:30 +0700

    feat: create footer

---
+++ src/components/ui/footer/index.tsx
@@ -0,0 +1,24 @@
+﻿import configApp from '@/configs/app';
+import styles from './Footer.module.scss';
+import Link from 'next/link';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.container}>
+      <div>Copyright © 2024. All rights are reserved</div>
+      <div className={styles.socials}>
+        {configApp.hero.socials.map((social, idx) => (
+          <Link
+            href={social.link}
+            key={idx}
+            target="_blank"
+          >
+            {social.icon}
+          </Link>
+        ))}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -3,5 +3,6 @@ import Hero from './home-page/hero';
 import About from './home-page/about';
 import ProjectList from './home-page/project';
 import Contact from './home-page/contact';
+import Footer from './footer';
 
-export { Header, Hero, About, ProjectList, Contact };
+export { Header, Hero, About, ProjectList, Contact, Footer };
